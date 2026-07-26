96cc8e11996e2e70e3e1b683ca88d6988894ec81
senegalha <22007659+senegalha@users.noreply.github.com>
2026-07-26 17:19:32 -0300

    feat: add custom exceptions

---
+++ api/src/main/java/com/gemstoneseekers/exceptions/EntityNotFoundException.java
@@ -0,0 +1,21 @@
+package com.gemstoneseekers.exceptions;
+
+@SuppressWarnings("PMD.MissingSerialVersionUID")
+public class EntityNotFoundException extends RuntimeException {
+    private final String entityName;
+    private final Object id;
+
+    public EntityNotFoundException(String entityName, Object id) {
+        super(entityName + " with id " + id + " not found");
+        this.entityName = entityName;
+        this.id = id;
+    }
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public Object getId() {
+        return id;
+    }
+}
