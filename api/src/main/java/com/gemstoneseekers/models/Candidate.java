02c2435455918d2786e80322fabd0727a3a5d91a
wellpaper23 <wevelle98@gmail.com>
2026-08-01 19:43:56 -0300

    feat(models): add candidateLink model entity

--- api/src/main/java/com/gemstoneseekers/models/Candidate.java
+++ api/src/main/java/com/gemstoneseekers/models/Candidate.java
@@ -1,17 +1,11 @@
 package com.gemstoneseekers.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.UuidGenerator;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,6 +34,9 @@ public class Candidate extends BaseModel {
 
     @Column(name = "phone")
     private String phone;
+
+    @OneToMany(mappedBy = "candidate", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CandidateLink> links;
 
     @Column(name = "summary")
     private String summary;
---
+++ api/src/main/java/com/gemstoneseekers/models/CandidateLink.java
@@ -0,0 +1,45 @@
+package com.gemstoneseekers.models;
+
+import java.util.UUID;
+
+import com.gemstoneseekers.models.BaseModel;
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "candidate_links")
+@Getter
+@Setter
+@NoArgsConstructor
+public class CandidateLink extends BaseModel {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "candidate_id", nullable = false)
+    private Candidate candidate;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "url", nullable = false, columnDefinition = "TEXT")
+    private String url;
+
+
+}
