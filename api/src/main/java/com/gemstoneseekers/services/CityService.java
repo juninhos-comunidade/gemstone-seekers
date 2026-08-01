02be3c150790edc94073c24e87eecc9b1476717b
senegalha <22007659+senegalha@users.noreply.github.com>
2026-07-31 22:51:26 -0300

    refactor: extract CityMapper and move mapping to controller

--- api/src/main/java/com/gemstoneseekers/services/CityService.java
+++ api/src/main/java/com/gemstoneseekers/services/CityService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.gemstoneseekers.dtos.response.CityResponse;
 import com.gemstoneseekers.models.City;
 import com.gemstoneseekers.repositories.CityRepository;
 
@@ -17,13 +16,11 @@ public class CityService {
         this.cityRepository = cityRepository;
     }
 
-    public List<CityResponse> getCities() {
-        List<City> cities = cityRepository.findAll();
-        return cities.stream().map(c -> new CityResponse(c.getId(), c.getName(), c.getState().getId())).toList();
+    public List<City> getCities() {
+        return cityRepository.findAll();
     }
 
-    public List<CityResponse> getCitiesByStateId(Integer stateId) {
-        List<City> cities = cityRepository.findByStateId(stateId);
-        return cities.stream().map(c -> new CityResponse(c.getId(), c.getName(), c.getState().getId())).toList();
+    public List<City> getCitiesByStateId(Integer stateId) {
+        return cityRepository.findByStateId(stateId);
     }
 }
