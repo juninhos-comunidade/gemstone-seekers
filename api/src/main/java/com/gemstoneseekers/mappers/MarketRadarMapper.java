280357bb4d285b95e372dc9937711fe3407ebaa9
senegalha <22007659+senegalha@users.noreply.github.com>
2026-08-02 10:28:43 -0300

    feat: add TechnologyDemandResponse DTO, MarketRadarMapper and MarketRadarService

---
+++ api/src/main/java/com/gemstoneseekers/mappers/MarketRadarMapper.java
@@ -0,0 +1,28 @@
+package com.gemstoneseekers.mappers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.gemstoneseekers.dtos.response.TechnologyDemandResponse;
+import com.gemstoneseekers.repositories.projections.TechnologyDemandProjection;
+
+@Component
+public class MarketRadarMapper {
+
+    public TechnologyDemandResponse toResponse(TechnologyDemandProjection projection) {
+        return new TechnologyDemandResponse(
+            projection.getTechnologyId(),
+            projection.getTechnologyName(),
+            projection.getTechnologyCategory(),
+            projection.getJobCount(),
+            projection.getMandatoryCount()
+        );
+    }
+
+    public List<TechnologyDemandResponse> toResponseList(List<TechnologyDemandProjection> projections) {
+        return projections.stream()
+            .map(this::toResponse)
+            .toList();
+    }
+}
---
+++ api/src/main/java/com/gemstoneseekers/services/MarketRadarService.java
@@ -0,0 +1,29 @@
+package com.gemstoneseekers.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.gemstoneseekers.dtos.response.TechnologyDemandResponse;
+import com.gemstoneseekers.enums.JobStatus;
+import com.gemstoneseekers.mappers.MarketRadarMapper;
+import com.gemstoneseekers.repositories.JobTechnologyRepository;
+
+@Service
+public class MarketRadarService {
+
+    private final JobTechnologyRepository jobTechnologyRepository;
+    private final MarketRadarMapper marketRadarMapper;
+
+    public MarketRadarService(JobTechnologyRepository jobTechnologyRepository,
+        MarketRadarMapper marketRadarMapper) {
+        this.jobTechnologyRepository = jobTechnologyRepository;
+        this.marketRadarMapper = marketRadarMapper;
+    }
+
+    public List<TechnologyDemandResponse> getTechnologyDemand() {
+        return marketRadarMapper.toResponseList(
+            jobTechnologyRepository.findTechnologyDemandByJobStatus(JobStatus.OPEN)
+        );
+    }
+}
