b969b495ab20fffedab0e423b4d28d608947b42e
wellpaper23 <wevelle98@gmail.com>
2026-08-16 18:18:59 -0300

    feat(user): add document uniqueness validation on registration

--- api/src/main/java/com/gemstoneseekers/repositories/UserRepository.java
+++ api/src/main/java/com/gemstoneseekers/repositories/UserRepository.java
@@ -14,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+    boolean existsByDocumentNumberAndDocumentType(String documentNumber, String documentType);
 }
